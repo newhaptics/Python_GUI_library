f08593ea142ee2e8ef85d2aae1ce330af7ad881f
NewHaptics <djjoslin@umic.edu>
2020-07-28 14:25:14 -0400

    Arduino testing {Failed Version}

--- Arduino_FC_Engine/libraries/FC_mechanics/FC_mechanics.cpp
+++ Arduino_FC_Engine/libraries/FC_mechanics/FC_mechanics.cpp
@@ -2,23 +2,27 @@
 #include "FC_mechanics.h"
 
 //input the rows and columns of the chip and record the ts, th, pw required to make a valid input
-FC_mechanics::FC_mechanics(int rows, int columns,int ts[][], int th[][], int tpw[][])) {
+FC_mechanics::FC_mechanics(int rows, int columns,int ts[20][20], int th[20][20], int tpw[20][20]) {
   this->rows = rows;
   this->columns = columns;
-  this->ts[0][0] = ts;
-  this->th[0][0] = th;
-  this->tpw[0][0] = tpw;
+  for(int i = 0; i < rows; i++){
+    for(int j = 0; j < columns; j++){
+      this->ts[i][j] = ts[i][j];
+      this->th[i][j] = th[i][j];
+      this->tpw[i][j] = tpw[i][j];
+    }
+  }
 
   //initialize data and latch lines
   //initialize latch lines as even numbers 22 to 52
-  for (int latchPin = 0; latchPin < rows && (latchPin + 22 =< 52); latchPin += 2) {
-    latchPins[latchPin] = latchPin + 22;
+  for (int latchPin = 0; latchPin/2 < rows && (latchPin + 22 <= 52); latchPin += 2) {
+    latchPins[latchPin/2] = latchPin + 22;
     pinMode(latchPins[latchPin], INPUT);
   }
 
   //initialize data lines as odd numbers 23 to 53
-  for (int dataPin = 0; dataPin < columns && (dataPin + 23 =< 53); dataPin += 2) {
-    dataPins[dataPin] = dataPin + 23;
+  for (int dataPin = 0; dataPin/2 < columns && (dataPin + 23 <= 53); dataPin += 2) {
+    dataPins[dataPin/2] = dataPin + 23;
     pinMode(dataPins[dataPin], INPUT);
   }
 
@@ -30,35 +34,41 @@ bool FC_mechanics::check_inputs() {
   int L = 0;
   int D = 0;
 
+  //int elapsed_time;
+
   //get the latch and data pin states
   for(int i = 0; i < rows; i++) {
+    //elapsed_time = micros();
     L = digitalRead(latchPins[i]);
-    if (latchPin_states[i][0] != L) {
+    //Serial.print("time to digitalRead latch "); Serial.println(micros() - elapsed_time);
+    if (latchPin_state[i] != L) {
+      Serial.print(latchPin_state[i]); Serial.print("->"); Serial.print(L); Serial.print("latchPin"); Serial.println(latchPins[i]);
+      //Serial.print("at "); Serial.println(millis());
       //if the previous state is not the same state push the state change
-      latchPin_times[i][1] = latchPin_times[i][0];
-      latchPin_times[i][0] = millis();
-      latchPin_states[i][1] = latchPin_states[i][0];
-      latchPin_states[i][0] = L;
+      latchPin_time[i] = millis();
     }
+    latchPin_state[i] = L;
   }
   for(int i = 0; i < columns; i++) {
+    //elapsed_time = micros();
     D = digitalRead(dataPins[i]);
-    if (dataPin_states[i] != D) {
+    //Serial.print("time to digitalRead latch "); Serial.println(micros() - elapsed_time);
+    if (dataPin_state[i] != D) {
+      Serial.print(dataPin_state[i]); Serial.print("->"); Serial.print(D); Serial.print("dataPin"); Serial.println(dataPins[i]);
+      //Serial.print("at "); Serial.println(millis());
       //if the previous state is not the same state record the time of change
-      dataPin_times[i][1] = dataPin_times[i][0];
-      dataPin_times[i][0] = millis();
-      dataPin_states[i][1] = dataPin_states[i][0];
-      dataPin_states[i][0] = D;
+      dataPin_time[i] = millis();
     }
+    dataPin_state[i] = D;
   }
 
   int new_state;
   bool state_change = false;
   //get the state changes based on the the data and latch change times
-  for(int row = 0; i < rows; row++){
-    for(int column = 0; i < columns; column++){
+  for(int row = 0; row < rows; row++){
+    for(int column = 0; column < columns; column++){
 
-      new_state = state_updater(state[row][column], ts[row][column], th[row][column], tpw[row][column], latchPin_times[row][0],latchPin_times[row][1],dataPin_times[column][0],dataPin_times[column][1],dataPin_states[column][0],dataPin_states[column][1],latchPin_states[row][0],latchPin_states[row][1]);
+      new_state = state_updater(state[row][column],ts[row][column],th[row][column],tpw[row][column],dataPin_time[column],latchPin_time[row],dataPin_state[column],latchPin_state[row]);
       //if the new state is different signal a state change
       if (new_state != state[row][column]){
         state[row][column] = new_state;
@@ -67,42 +77,45 @@ bool FC_mechanics::check_inputs() {
 
     }
   }
-  
-  return state_change
+
+  return state_change;
 
 }
 
 
 //state updater
-int FC_mechanics::state_updater(int state ,int minS, int minH, int minPW, int latchEdge1, int latchEdge2, int dataEdge1, int dataEdge2, int latchState1, int latchState2, int dataState1, int dataState2){
-
-
-  //first deteremine if latch went low or HIGH
-  if (latchState1 > latchState2) {
+int FC_mechanics::state_updater(int currentState, int minS, int minH, int minPW, int dataEdge, int latchEdge, int dataState, int latchState){
+  //Serial.print("state "); Serial.print(state); Serial.print("| latchEdge1 "); Serial.print(latchEdge1); Serial.print("| latchEdge2 "); Serial.print(latchEdge2); Serial.print("| dataEdge1 "); Serial.print(dataEdge1); Serial.print("| dataEdge2 "); Serial.print(dataEdge2); Serial.print("| latchState1 "); Serial.print(latchState1); Serial.print("| latchState2 "); Serial.print(latchState2); Serial.print("| dataState1 "); Serial.print(dataState1); Serial.print("| dataState2 "); Serial.println(dataState2);
+  int latchSteady_time = millis() - latchEdge;
+  int dataSteady_time = millis() - dataEdge;
     //calculate the current setup, hold, and pw
-    int s = latchEdge1 - dataEdge1;
-    int h = dataEdge2 - latchEdge1;
-    int pw = latchEdge2 - latchEdge1;
-
-    //make sure that the setup, hold, and pulse width fit within the physical contraints of the FC
-    if(s > minS && h > minH && pw > minPW){
-      //if the physical constraints fit then change state
-      return dataState2
+    int s = latchEdge - dataEdge;
+    int h;
+    if (dataEdge > latchEdge){
+      h = dataSteady_time - s;
     } else {
-      return state
+      h = millis() - latchEdge;
+    }
+    int pw = latchSteady_time;
+
+    if (latchState == 0){
+      Serial.print("state "); Serial.print(currentState); Serial.print("| latchEdge "); Serial.print(latchEdge); Serial.print("| dataEdge "); Serial.print(dataEdge); Serial.print("| dataState "); Serial.print(dataState); Serial.print("| latchState "); Serial.println(latchState);
+      Serial.print("latchSteady_time "); Serial.print(latchSteady_time); Serial.print("| dataSteady_time "); Serial.print(dataSteady_time); Serial.print("| s "); Serial.print(s); Serial.print("| h "); Serial.print(h); Serial.print("| pw "); Serial.println(pw);
     }
 
-  } else {
-    return state
-  }
+    //make sure that the setup, hold, and pulse width fit within the physical contraints of the FC
+    if(s > minS && h > minH && pw > minPW && latchState == 0){
+      //if the physical constraints fit then change state
+      return dataState;
+    } else {
+      return currentState;
+    }
 
 }
 
 
-
-}
 
 //give the current state of the device
-int[][] FC_mechanics::get_state(){
-  return this->state[rows][columns]
+int FC_mechanics::get_state(int row, int column){
+  return state[row][column];
 }
--- Arduino_FC_Engine/libraries/FC_mechanics/FC_mechanics.h
+++ Arduino_FC_Engine/libraries/FC_mechanics/FC_mechanics.h
@@ -10,15 +10,15 @@ class FC_mechanics {
 
 public:
   //initialize size of chip
-  FC_mechanics(int rows, int columns,int ts[][], int th[][], int tpw[][]));
+  FC_mechanics(int rows, int columns,int ts[20][20], int th[20][20], int tpw[20][20]);
 
   //update state of the chip
   //checks if the latch and data line have been on long enough to change state
   //if valid input change state
-  bool check_inputs();
+  bool check_inputs(void);
 
-  //get the state of the chip
-  int[][] get_state();
+  //get index of the state of the chip
+  int get_state(int row, int column);
 
 
 private:
@@ -28,28 +28,28 @@ private:
 
 
   //internal state of the chip
-  int state [100][100];
+  int state [20][20];
 
 
   //benchmark data of the chip
-  int ts[100][100];
-  int th[100][100];
-  int tpw[100][100];
+  int ts[20][20];
+  int th[20][20];
+  int tpw[20][20];
 
   //arrays for latch and data pin locations
-  int latchPins[100];
-  int dataPins[100];
+  int latchPins[20];
+  int dataPins[20];
 
   //arrays for last time of pin rise/fall edge
-  int latchPin_times[100][2];
-  int dataPin_times[100][2];
+  int latchPin_time[20];
+  int dataPin_time[20];
 
   //arrays for last time of pin state change
-  int latchPin_states[100][2];
-  int dataPin_states[100][2];
+  int latchPin_state[20];
+  int dataPin_state[20];
 
-  //state change calculator that takes the change times for both and their current state and the previous state and time
-  int state_updater(int state, int minS, int minH, int minPW, int latchEdge1, int latchEdge2, int dataEdge1, int dataEdge2, int latchState1, int latchState2, int dataState1, int dataState2);
+  //takes in the time that latch has been open if it is open and the last edge of data
+  int state_updater(int currentState, int minS, int minH, int minPW, int dataEdge, int latchEdge, int dataState, int latchState);
 
   };
 
